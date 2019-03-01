625527d76d34acb3bb4cfe497c8f9fb9497f6ce4
Jucavi <jcarvil@gmail.com>
2019-03-01 23:06:36 +0100

    Modify name.rb to output full name

---
+++ variables_exercises/3/name.rb
@@ -0,0 +1,6 @@
+puts "Enter your first name:"
+first_name = gets.chomp
+puts "Enter your last name:"
+last_name = gets.chomp
+
+puts "Your full name: " + first_name.capitalize + " " + last_name.capitalize
