c8ad9cdf10c9097bfa0a0d42c85aff29e3bfb2ef
Jucavi <jcarvil@gmail.com>
2019-03-01 22:39:21 +0100

    Add solution 3

---
+++ basics_exercises/3.rb
@@ -0,0 +1,13 @@
+movies = {
+  movie1: 1960,
+  movie2: 1970,
+  movie3: 1980,
+  movie4: 1990,
+  movie5: 2000
+}
+
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+puts movies[:movie4]
+puts movies[:movie5]
