0fa7dfac04bb93125d64635e74cc18dc1c140a09
Jucavi <jcarvil@gmail.com>
2019-03-01 22:35:01 +0100

    Add solution 2

---
+++ basics_exercises/2.rb
@@ -0,0 +1,8 @@
+number = 1234
+
+thousands = number / 1000
+hundreds = (number % 1000) / 100
+tens = (number % 100 ) / 10
+ones = number % 10
+
+# puts "#{thousands} : #{hundreds} : #{tens} : #{ones}"
