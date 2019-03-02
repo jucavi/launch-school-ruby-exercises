60644cade441aaa9bfb83669db3661a10ea6fded
Jucavi <jcarvil@gmail.com>
2019-03-02 16:32:12 +0100

    Add Flow Control chapter solutions

---
+++ flow_control_exercises/6/equal_to_four.rb
@@ -0,0 +1,19 @@
+# def equal_to_four(x)
+#   if x == 4
+#     puts "yup"
+#   else
+#     puts "nope"
+# end
+
+# equal_to_four(5)
+# => exercise.rb:8: syntax error, unexpected end-of-input, expecting keyword_end
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
